fd6ab4c82b5042446a30ae36fca463815550dd67
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-08-23 23:25:49 +0430

    custom action

--- apps/account/admin.py
+++ apps/account/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+
+@admin.action(description="Covert to ADMIN level")
+def change_to_admin(modeladmin, request, queryset):
+    queryset.update(level="ADMIN")
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "email", "phone_number", "level")
+    list_per_page = 25
+    list_editable = ("level",)
+    list_filter = ("level", "created_at")
+    actions = (change_to_admin, )
+
+
+admin.site.register(get_user_model(), UserAdmin)
