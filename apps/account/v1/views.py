7fcc3c50ddeac0e8ee8a0e39c1dbc3e204b08467
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-08-28 19:21:26 +0430

    rewrite register and add token validation views

--- apps/account/v1/views.py
+++ apps/account/v1/views.py
@@ -1,21 +1,57 @@
+import json
+
 from django.contrib.auth import get_user_model
+from django.db.models import Q
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.account.v1.serializers import UserSerializer
+from apps.account.v1.serializers import (
+    RegisterSerializer,
+    TokenSerializer,
+    UserSerializer
+)
 
 
-class CreateUser(APIView):
-    serializer = UserSerializer
-    model = get_user_model()
+class RegisterUserView(APIView):
+    serializer = RegisterSerializer
 
     def post(self, request):
         serializer = self.serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            token = serializer.create(validated_data=serializer.data)
+            print("token -> ", token)
+            return Response({}, status=status.HTTP_200_OK)
+
+
+class TokenSerializerView(APIView):
+    serializer = TokenSerializer
+    model = get_user_model()
+
+    def put(self, request):
+        serializer = self.serializer(data=request.data)
 
         if serializer.is_valid(raise_exception=True):
-            serializer.save()
+            print(serializer.data)
+            if result := serializer.create(validated_data=serializer.data):
+                if result:
 
-        return Response(serializer.data)
+                    user = get_user_model().objects.filter(
+                        Q(phone_number=serializer.data.get("phoneNumber", None)) |
+                        Q(email=serializer.data.get("email", None))
+                    ).first()
+
+                    if not user:
+                        user = self.model()
+                        if phone_number := serializer.data.get("phoneNumber", None):
+                            user.phone_number = phone_number
+                        if email := serializer.data.get("email", None):
+                            user.email = email
+                        user.save()
+                        return Response({"phoneNumber": user.phone_number, "email": user.email, "level": user.level}, status=status.HTTP_201_CREATED)
+                    return Response({"phoneNumber": user.phone_number, "email": user.email, "level": user.level}, status=status.HTTP_200_OK)
+                return Response({"msg": "Code was wrong"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"msg": "Token does not exist."}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class ListUsers(APIView):
