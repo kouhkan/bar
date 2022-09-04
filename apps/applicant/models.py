d84b13243ad412004d7dc854eebac4be1694abf9
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-09-04 20:30:02 +0430

    customize admin panel

--- apps/applicant/models.py
+++ apps/applicant/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class ApplicaStatus(models.TextChoices):
+    SEND = "SEND"
+    ACCEPTED = "ACCEPTED"
+
+
+class Applicant(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="applicants"
+    )
+    title = models.CharField(max_length=255, null=False, unique=False)
+    slug = models.SlugField(max_length=255, null=False, unique=True)
+    description = models.TextField()
+    location = models.TextField()
+    status = models.CharField(
+        max_length=10,
+        null=False,
+        choices=ApplicaStatus.choices,
+        default="SEND",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.id}"
