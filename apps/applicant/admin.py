ccc68c74ae9fc9eb921a8eacc1e0290cda49388f
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-09-04 20:30:26 +0430

    create model

--- apps/applicant/admin.py
+++ apps/applicant/admin.py
@@ -1,3 +1,22 @@
+from typing import Dict, Sequence
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Applicant
+
+
+class ApplicantAdmin(admin.ModelAdmin):
+    list_display: Sequence[str] = ("id", "user_id", "title", "status")
+    list_filter: Sequence[str] = ("status",)
+    list_editable: Sequence[str] = ("status",)
+    list_per_page: int = 25
+    search_fields: Sequence[str] = (
+        "title",
+        "description",
+        "user_id",
+        "location",
+    )
+    prepopulated_fields: Dict[str, Sequence[str]] = {"slug": ("title",)}
+
+
+admin.site.register(Applicant, ApplicantAdmin)
