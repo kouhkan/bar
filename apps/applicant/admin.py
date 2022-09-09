f2f345b6f000ff67904448f7e97a04404d440f71
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-09-09 15:01:00 +0430

    add soft delete to admin

--- apps/applicant/admin.py
+++ apps/applicant/admin.py
@@ -2,9 +2,12 @@ from typing import Dict, Sequence
 
 from django.contrib import admin
 
+from .models import DeletedApplicant
 from .models import Applicant
+from .actions import recovery
 
 
+@admin.register(Applicant)
 class ApplicantAdmin(admin.ModelAdmin):
     list_display: Sequence[str] = ("id", "user_id", "title", "status")
     list_filter: Sequence[str] = ("status",)
@@ -19,4 +22,20 @@ class ApplicantAdmin(admin.ModelAdmin):
     prepopulated_fields: Dict[str, Sequence[str]] = {"slug": ("title",)}
 
 
-admin.site.register(Applicant, ApplicantAdmin)
+@admin.register(DeletedApplicant)
+class DeletedApplicantAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return DeletedApplicant.deleted.filter(is_deleted=True)
+
+    actions = (recovery,)
+    list_display: Sequence[str] = ("id", "user_id", "title", "deleted_at", "status")
+    list_filter: Sequence[str] = ("status",)
+    list_editable: Sequence[str] = ("status",)
+    list_per_page: int = 25
+    search_fields: Sequence[str] = (
+        "title",
+        "description",
+        "user_id",
+        "location",
+    )
+    prepopulated_fields: Dict[str, Sequence[str]] = {"slug": ("title",)}
