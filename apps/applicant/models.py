600342d337650c0329d108f5f637955872ca3226
Amir Kouhkan <amirkouhkan1@gmail.com>
2022-09-09 15:02:24 +0430

    add deleted proxy model for applicants

--- apps/applicant/models.py
+++ apps/applicant/models.py
@@ -1,15 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.db.models import Manager
+
+from utils.db.base import BaseModel
+from utils.db.soft_delete import SoftDelete
 
 User = get_user_model()
 
 
-class ApplicaStatus(models.TextChoices):
+class ApplicantStatus(models.TextChoices):
     SEND = "SEND"
     ACCEPTED = "ACCEPTED"
 
 
-class Applicant(models.Model):
+class Applicant(BaseModel, SoftDelete):
     user_id = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="applicants"
     )
@@ -20,9 +24,19 @@ class Applicant(models.Model):
     status = models.CharField(
         max_length=10,
         null=False,
-        choices=ApplicaStatus.choices,
+        choices=ApplicantStatus.choices,
         default="SEND",
     )
 
+    class Meta:
+        default_manager_name = "objects"
+
     def __str__(self) -> str:
         return f"{self.id}"
+
+
+class DeletedApplicant(Applicant):
+    deleted = Manager()
+
+    class Meta:
+        proxy = True
